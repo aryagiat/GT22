2da924e8958b48e763b45f2c1badb1b6b4bac0cc
aryagiat <aryagiat@connect.hku.hk>
2020-04-07 11:59:52 +0700

    start menu testing cpp file

---
+++ start.cpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <cstdio> //exit
+#include <windows.h> // system
+#include <unistd.h> //system
+#include <conio.h> //getch
+
+using namespace std;
+
+
+char start_screen[23][37] = {
+  "====================================",
+  "|   -           __                 |",
+  "| --          ~( @\\   \\            |",
+  "|---   _________]_[__/_>________   |",
+  "|     /  ____ \\ <>     |  ____  \\  |",
+  "|    =\\_/ __ \\_\\_______|_/ __ \\__D |",
+  "|________(__)_____________(__)_____|",
+  "|                                  |",
+  "|        _______________  ___      |",
+  "|       / ____/_  __/__ \\|__ \\     |",
+  "|      / / __  / /  __/ /__/ /     |",
+  "|     / /_/ / / /  / __// __/      |",
+  "|     \\____/ /_/  /____/____/      |",
+  "|                                  |",
+  "|                                  |",
+  "|             START                |",
+  "|             GARAGE               |",
+  "|             INSTRUCTIONS         |",
+  "|             EXIT                 |",
+  "|                                  |",
+  "|      use up and down arrow       |",
+  "|      press enter to select       |",
+  "===================================="
+};
+
+void clearStar();
+void DetectKey(int &option);
+
+int main()
+{
+  
+  // options: 0 = Start, 1 = Car select, 2 = Instruction, 3 = Exit
+  int option = 0;
+  while(1){
+    system("tput clear");
+    // Printing out the screen
+    for (int i = 0; i < 23; i++){
+      cout << start_screen[i] << endl;
+    }
+
+    switch(option){
+      case(0):
+        start_screen[15][10] = '*';
+        start_screen[15][11] = '*';
+        start_screen[15][12] = '*';
+        break;
+      case(1):
+        start_screen[16][10] = '*';
+        start_screen[16][11] = '*';
+        start_screen[16][12] = '*';
+        break;
+      case(2):
+        start_screen[17][10] = '*';
+        start_screen[17][11] = '*';
+        start_screen[17][12] = '*';
+        break;
+      case(3):
+        start_screen[18][10] = '*';
+        start_screen[18][11] = '*';
+        start_screen[18][12] = '*';
+        break;
+      default:
+        start_screen[15][10] = '*';
+        start_screen[15][11] = '*';
+        start_screen[15][12] = '*';
+        break;
+    }
+    system("pause>nul");
+    // Check which key is being pressed
+    if (GetAsyncKeyState(VK_UP)){
+      if (option != 0)
+        option--;
+      clearStar();
+    }
+    if (GetAsyncKeyState(VK_DOWN)){
+      if (option != 3)
+        option++;
+      clearStar();
+    }
+  }
+  return 0;
+}
+
+void clearStar(){
+  start_screen[15][10] = ' ';
+  start_screen[15][11] = ' ';
+  start_screen[15][12] = ' ';
+
+  start_screen[16][10] = ' ';
+  start_screen[16][11] = ' ';
+  start_screen[16][12] = ' ';
+
+  start_screen[17][10] = ' ';
+  start_screen[17][11] = ' ';
+  start_screen[17][12] = ' ';
+
+  start_screen[18][10] = ' ';
+  start_screen[18][11] = ' ';
+  start_screen[18][12] = ' ';
+}
+
+void DetectKey(int &option){
+  int key;
+  key = getch();
+
+  if (key == 72){
+    if (option != 0)
+      option--;
+    clearStar();
+  }
+  if (key == 80){
+    if (option != 3)
+      option++;
+    clearStar();
+  }
+}
