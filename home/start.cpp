d83c7214fbd5a8bb9ac7ed543677f57899c0d0c8
Giat Arya Nagatama <aryagiat@connect.hku.hk>
2020-04-12 19:01:17 +0800

    Start.cpp done, just need garage function

--- home/start.cpp
+++ home/start.cpp
@@ -11,10 +11,82 @@ using namespace std;
 const int ROW = 25;
 const int COL = 37;
 
-void ClearArrow(char screen[ROW][COL]);
-void PerformAction(int option, char screen[][COL]);
+// Function protoypes.
+void ReadArt(char screen[][COL]); // Function to read the start screen art. The input is a the screen map
+void ReadStat(char screen[][COL]); // Function to read highscore and coin. This function also updates the screen map which outputs the highscore and coin. The input is the screen map.
+void ClearArrow(char screen[][COL]); // Function to clear the arrow on the screen by making column 11 a blank space on rows where there are options. The input of this function is the screen art map.
+void PerformAction(int option, char screen[][COL]); // Function that performs action depending on the option selected by user. The input of this function is the option that the user selects and the screen map.
 
+
+
+
+// ==================================
+// === main function of start.cpp ===
 int start(){
+  char screen[ROW][COL]; // screen made out of a 25 by 37 character map
+  ReadArt(screen); // reading from startart.txt
+
+  int option = 1;
+  // Selecting the options from the start menu.
+  // 1: Start playing, 2: Open garage, 3: View Instructions, 4: Reset Progress, 5: Exit game
+  while (true){
+    ReadStat(screen); // Reading highscore and coin and update to screen.
+    system("clear"); //Clearing console screen.
+    // Printing the start art.
+    for (int i = 0; i < 25; i++){
+      cout << screen[i] << endl;
+    }
+
+    // Detecting keyboard presses and perform actions.
+    char key;
+    key = keylog();
+    switch (key){
+      // w button to navigate up
+      case 'w':
+      {
+        if (option != 1)
+          option--;
+        ClearArrow(screen); // Clear the arrow
+        screen[15+option][11] = '>'; // Move the arrow up
+        break;
+      }
+
+      // s button to navigate down
+      case 's':
+      {
+        if (option != 5)
+          option++;
+        ClearArrow(screen); // Clear the arrow
+        screen[15+option][11] = '>'; // Move the arrow down
+        break;
+      }
+      // enter button to perform actions depending on the option.
+      case '\n':
+      {
+        if (option == 1){
+          // Start Playing.
+          return 0;
+        }
+        else{
+          // Perform other actions on the start screen.
+          PerformAction(option, screen);
+        }
+        break;
+      }
+    } // end of switch
+  } // end of while loop
+  return 0;
+}
+// ====== END of start function =====
+// ==================================
+
+// =============================================================================
+
+//===================================
+// ============ Functions ===========
+
+// Function to read the start screen art. The input is a the screen map
+void ReadArt(char screen[][COL]){
   // Opening the start art graphics design .txt file
   ifstream art;
   art.open("./home/startart.txt");
@@ -23,17 +95,19 @@ int start(){
     exit(1);
   }
 
-  // Outputing the graphics design into the console
-  char screen[ROW][COL]; // screen made out of a 25 by 37 character map
+  // Storing the graphics design into the screen map
   string str;
-  int ind = 0;
+  int art_index = 0;
   // reading from the startart.txt file
   while (getline(art, str)){
-    strcpy(screen[ind], str.c_str()); // converting string into a char array
-    ind++;
+    strcpy(screen[art_index], str.c_str()); // converting string into a char array
+    art_index++;
   }
   art.close();
+}
 
+// Function to read highscore and coin. This function also updates the screen map which outputs the highscore and coin. The input is the screen map.
+void ReadStat(char screen[][COL]){
   // Opening the stat file to see highscore and coins.
   ifstream stat;
   stat.open("./home/stat.txt");
@@ -47,6 +121,13 @@ int start(){
   stat.close();
   int highscore_size = highscore.size();
   int coin_size = coin.size();
+  // Clearing the space to outpupt the highscore and coin to the screen.
+  for (int i = 14; i < COL - 2; i++){
+    screen[1][i] = ' ';
+  }
+  for (int i = 8; i < COL - 2; i++){
+    screen[2][i] = ' ';
+  }
   // Replacing the empty space on the screen with a highscore value
   for (int i = 0; i < highscore_size; i++){
     screen[1][i + 14] = highscore[i];
@@ -55,55 +136,6 @@ int start(){
   for (int i = 0; i < coin_size; i++){
     screen[2][i + 8] = coin[i];
   }
-
-
-  int option = 1;
-  // Selecting the options from the start menu.
-  // 1: Start playing, 2: Open garage, 3: View Instructions, 4: Reset Progress, 5: Exit game
-  while (true){
-    system("clear"); //Clearing console screen.
-    // Printing the start art
-    for (int i = 0; i < 25; i++){
-      cout << screen[i] << endl;
-    }
-
-    // Detecting keyboard presses
-    char key;
-    key = keylog();
-    switch (key){
-      // w button to navigate up
-      case 'w':
-        if (option != 1)
-          option--;
-        ClearArrow(screen);
-        screen[15+option][11] = '>';
-        break;
-
-      // s button to navigate down
-      case 's':
-        if (option != 5)
-          option++;
-        ClearArrow(screen);
-        screen[15+option][11] = '>';
-        break;
-
-      // enter button to perform actions depending on the option.
-      case '\n':
-        if (option == 1){
-          return 0;
-        }
-        else{
-          PerformAction(option, screen);
-        }
-        break;
-    }
-  }
-  return 0;
-}
-
-// Function to read the start screen art. The input is a the screen map
-void ReadArt(char screen[][COL]){
-  
 }
 
 // Function to clear the arrow on the screen by making column 11 a blank space on rows where there are options. The input of this function is the screen art map.
@@ -113,7 +145,7 @@ void ClearArrow(char screen[ROW][COL]){
   }
 }
 
-// Function that performs action depending on the option selected by user. The input of this function is the option that the user selects.
+// Function that performs action depending on the option selected by user. The input of this function is the option that the user selects and the screen map.
 void PerformAction(int option, char screen[][COL]){
   char confirm;
   switch (option){
@@ -202,3 +234,6 @@ void PerformAction(int option, char screen[][COL]){
     }
   }
 }
+
+// ======== End of Functions ========
+// ===================================
